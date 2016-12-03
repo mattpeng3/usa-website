509052acb0b9fa35b5f32c9c344d0fb942432605
David Nahm <davidsnahm@berkeley.edu>
2016-12-03 00:09:29 -0800

    added stuff

--- schedule_builder/urls.py
+++ schedule_builder/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name = 'index'),
+	url(r'stat-133', views.stat133, name = "stat133"),
+	url(r'stat-134', views.stat134, name = "stat134"),
+	url(r'stat-135', views.stat135, name = "stat135"),
+	url(r'people', views.people, name = "people"),
+	url(r'course-map', views.courseMap, name = "courseMap"),
+	url(r'project', views.project, name = "project")
 ]
--- schedule_builder/views.py
+++ schedule_builder/views.py
@@ -12,3 +12,32 @@ def index(request):
 	return HttpResponse(html, context)
 
 # Create your views here.
+def stat133(request):
+	t = get_template("stat-133.html")
+	context = Context({})
+	return HttpResponse(t.render(context))
+
+def stat134(request):
+	t = get_template("stat-134.html")
+	context = Context({})
+	return HttpResponse(t.render(context))
+
+def stat135(request):
+	t = get_template("stat-135.html")
+	context = Context({})
+	return HttpResponse(t.render(context))
+
+def people(request):
+	t = get_template("people.html")
+	context = Context({})
+	return HttpResponse(t.render(context))
+
+def courseMap(request):
+	t = get_template("course-map.html")
+	context = Context({})
+	return HttpResponse(t.render(context))
+
+def project(request):
+	t = get_template("project.html")
+	context = Context({})
+	return HttpResponse(t.render(context))
