c5199014a03e5fa77a9589ff61289a3b1790dccc
Yitz Deng <yitzdeng@berkeley.edu>
2017-02-28 11:49:51 -0800

    Added end-of-string indicators to each url in schedulebuidler/urls.py

--- schedule_builder/urls.py
+++ schedule_builder/urls.py
@@ -2,17 +2,17 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'index', views.index, name = 'index'),
-	url(r'home', views.home, name = 'home'),
+	url(r'index$', views.index, name = 'index'),
+	url(r'home$', views.home, name = 'home'),
 	url(r'^$', views.home, name = 'home'),
-	url(r'aboutus', views.aboutus, name = 'aboutus'),
-	url(r'stat-133', views.stat133, name = "stat133"),
-	url(r'stat-134', views.stat134, name = "stat134"),
-	url(r'stat-135', views.stat135, name = "stat135"),
-	url(r'people', views.people, name = "people"),
-	url(r'course-map', views.courseMap, name = "courseMap"),
-	url(r'project', views.project, name = "project"),
-        url(r'webdev', views.webdev, name="webdev"),
-        url(r'events', views.events, name="events"),
-        url(r'careerex', views.careerex, name="careerex")
+	url(r'aboutus$', views.aboutus, name = 'aboutus'),
+	url(r'stat-133$', views.stat133, name = "stat133"),
+	url(r'stat-134$', views.stat134, name = "stat134"),
+	url(r'stat-135$', views.stat135, name = "stat135"),
+	url(r'people$', views.people, name = "people"),
+	url(r'course-map$', views.courseMap, name = "courseMap"),
+	url(r'project$', views.project, name = "project"),
+        url(r'webdev$', views.webdev, name="webdev"),
+        url(r'events$', views.events, name="events"),
+        url(r'careerex$', views.careerex, name="careerex")
 ]
