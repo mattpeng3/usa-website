e266aa72f3ad1e57952599bf16a571650d925404
Anton Oyung <Anton.oyung@berkeley.edu>
2017-03-11 16:07:14 -0800

    Add 150 and 155 to urls.py

--- schedule_builder/urls.py
+++ schedule_builder/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . import views
+from schedule_builder import views
 
 urlpatterns = [
 	url(r'index$', views.index, name = 'index'),
@@ -9,6 +9,8 @@ urlpatterns = [
 	url(r'stat-133$', views.stat133, name = "stat133"),
 	url(r'stat-134$', views.stat134, name = "stat134"),
 	url(r'stat-135$', views.stat135, name = "stat135"),
+	url(r'stat-150$', views.stat150, name = "stat150"),
+	url(r'stat-155$', views.stat155, name = "stat155"),
 	url(r'people$', views.people, name = "people"),
 	url(r'course-map$', views.courseMap, name = "courseMap"),
 	url(r'project$', views.project, name = "project"),
