4f8d9692a0e0730d0eeccdc688da6505c16f299f
Undergraduate Statistics Association <ugradsa@ocf.berkeley.edu>
2017-08-29 12:01:20 -0700

    Made a few emergency updates when I presented the website for usa last spring

--- schedule_builder/models.py
+++ schedule_builder/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Course(models.Model):
 	name = models.CharField(max_length = 50)
 	
+
+class blogPost(models.Model):
+        title = models.CharField(max_length=50)
+        body = models.CharField(max_length=200)
+
