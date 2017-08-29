8adb9852aca6fa2fd3d9e6c4146ee1e153fb40be
Yitz Deng <yitzdeng@berkeley.edu>
2017-08-29 12:51:04 -0700

    blog structure start

--- schedule_builder/models.py
+++ schedule_builder/models.py
@@ -4,3 +4,15 @@ from django.db import models
 class Course(models.Model):
 	name = models.CharField(max_length = 50)
 	
+
+class Blog(models.Model):
+        title = models.CharField(max_length=100, unique=True)
+        slug = models.SlugField(max_length=100, unique=True)
+        body =models.TextField()
+        posted = models.DateField(db_index=100, unique=True)
+        category = models.ForeignKey('blog.Category')
+
+class Category(models.Model):
+        title = models.CharField(max_length=100, db_index=True)
+        slug = models.SlugField(max_length=100, db_index=True)
+
