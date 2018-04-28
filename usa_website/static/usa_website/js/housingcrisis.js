1fb4ec06f342c483698ee319cac2f7784bed30d0
Sheepinav <abhaskar@berkeley.edu>
2018-04-28 13:30:35 -0700

    last commit before the final version goes in

--- usa_website/static/usa_website/js/housingcrisis.js
+++ usa_website/static/usa_website/js/housingcrisis.js
@@ -178,7 +178,7 @@ legend.onAdd = function (map) {
 	var labels = []
 	/* Add min & max*/
 	div.innerHTML = '<div id="legend"><h3 style="font-weight:bolder;font-size:larger; text-align:center;">Preference Scale</h3></div>\
-		<div ><img src="colorscale.png" alt=""></div><div class="labels"><span class="domain-min">Low Pref</span>\
+		<div ><img src="{% static 'usa_website/images/housingcrisis/colorscale.png' %}" alt=""></div><div class="labels"><span class="domain-min">Low Pref</span>\
 		<span class="domain-max">High Pref</span>\
     </div>'
 	return div
