95a676c56ee230689ba60d93ac83679ca50f2301
Sheepinav <abhaskar@berkeley.edu>
2018-10-07 05:12:10 -0700

    trying to explicitly define redirect_uri

--- usa_website/utils/attendance.py
+++ usa_website/utils/attendance.py
@@ -25,6 +25,7 @@ except ImportError:
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
 CLIENT_SECRET_LOC = '/home/u/ug/ugradsa/usa-website/src/usa_website/utils/client_secret.json'
 APPLICATION_NAME = 'SusaClient'
+REDIRECT_URI = 'https://susa.berkeley.edu/attendance'
 #You wouldn't steal a car would you? So please don't steal our credentials! Thanks!
 
 def get_credentials():
@@ -41,6 +42,7 @@ def get_credentials():
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_LOC, SCOPES)
         flow.params['access_type'] = 'offline'
         flow.params['approval_prompt'] = 'force'
+        flow.params['redirect_uri'] = REDIRECT_URI
         print(flow.params['access_type'])
         if flags:
             credentials = tools.run_flow(flow, store, flags)
