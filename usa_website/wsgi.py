5b6e04cae7b173a92fd14600803fdded43976055
Sheepinav <abhaskar@berkeley.edu>
2018-10-19 21:15:40 -0700

    pathway

--- usa_website/wsgi.py
+++ usa_website/wsgi.py
@@ -15,7 +15,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "usa_website.settings")
 
 if __file__ == '/home/u/ug/ugradsa/usa-website/src/usa_website/wsgi.py':
-    activate_env=os.path.expanduser("~/usa-website/src/venv/bin/activate_this.py")
+    activate_env=os.path.expanduser("/home/u/ug/ugradsa/usa-website/src/venv/bin/activate_this.py")
     site.addsitedir('/home/u/ug/ugradsa/usa-website/src/venv/lib/python3.4/site-packages')
     print(activate_env)
     #execfile(activate_env, dict(__file__=activate_env))
