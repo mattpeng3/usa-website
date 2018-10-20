8458af7f689d7a88f715402b4ca7b0185da82a84
Sheepinav <abhaskar@berkeley.edu>
2018-10-19 22:20:44 -0700

    trying a diff way of finding cwd

--- usa_website/wsgi.py
+++ usa_website/wsgi.py
@@ -15,14 +15,14 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "usa_website.settings")
 logging.warning('test1')
-if __file__ == '/home/u/ug/ugradsa/usa-website/src/usa_website/wsgi.py':
+if os.path.dirname(os.path.abspath(__file__)) == '/home/u/ug/ugradsa/usa-website/src/usa_website':
     activate_env=os.path.expanduser("~/usa-website/src/venv2/bin/activate_this.py")
     site.addsitedir('~/usa-website/src/venv2/lib/python2.7/site-packages')
     execfile(activate_env, dict(__file__=activate_env))
 # else:
+#     print(os.path.dirname(os.path.abspath(__file__)))
 #     activate_env=os.path.expanduser("./venv/bin/activate_this.py")
 #     site.addsitedir('./venv/lib/python3.6/site-packages')
-#     print(activate_env)
 #     with open(activate_env) as f:
 #         exec(f.read(), {'__file__': activate_env})
 #add external libs in venv
