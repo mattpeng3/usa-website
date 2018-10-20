ef8384a96890af257e105a62f17961f967b7b459
Sheepinav <abhaskar@berkeley.edu>
2018-10-19 22:11:22 -0700

    if __file__ doesn't work, trying to fix

--- usa_website/wsgi.py
+++ usa_website/wsgi.py
@@ -15,7 +15,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "usa_website.settings")
 logging.warning('test1')
-if __file__ == '~/usa-website/src/usa_website/wsgi.py':
+if __file__ == '/home/u/ug/ugradsa/usa-website/src/usa_website/wsgi.py':
     activate_env=os.path.expanduser("~/usa-website/src/venv2/bin/activate_this.py")
     site.addsitedir('~/usa-website/src/venv2/lib/python2.7/site-packages')
     execfile(activate_env, dict(__file__=activate_env))
