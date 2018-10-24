87129355473e247eb5c01bac6978d22b2a8c08c8
Sheepinav <abhaskar@berkeley.edu>
2018-10-23 20:18:11 -0700

    adding virtualenv for users with updated virtualenv version

--- usa_website/wsgi.py
+++ usa_website/wsgi.py
@@ -14,13 +14,17 @@ import logging
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "usa_website.settings")
-logging.warning('test1')
+
 if os.path.dirname(os.path.abspath(__file__)) == '/home/u/ug/ugradsa/usa-website/src/usa_website':
     activate_env=os.path.expanduser("~/usa-website/src/venv2/bin/activate_this.py")
     site.addsitedir('~/usa-website/src/venv2/lib/python2.7/site-packages')
     execfile(activate_env, dict(__file__=activate_env))
+elif os.path.isdir(os.path.abspath(os.path.join(os.pardir,'venv/Scripts'))):
+    activate_env=os.path.expanduser("./venv/Scripts/activate_this.py")
+    site.addsitedir('./venv/lib/python3.6/site-packages')
+    with open(activate_env) as f:
+        exec(f.read(), {'__file__': activate_env})
 else:
-    print(os.path.dirname(os.path.abspath(__file__)))
     activate_env=os.path.expanduser("./venv/bin/activate_this.py")
     site.addsitedir('./venv/lib/python3.6/site-packages')
     with open(activate_env) as f:
