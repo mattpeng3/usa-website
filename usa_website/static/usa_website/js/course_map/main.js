eecd5909410c95210e80ce0b3ef9ad13d467ef4a
Anthony Yu <anthonyhsyu@berkeley.edu>
2018-11-29 23:22:05 -0800

    Attempt at course map changes

--- usa_website/static/usa_website/js/course_map/main.js
+++ usa_website/static/usa_website/js/course_map/main.js
@@ -19,7 +19,7 @@ function GetQueryStringParams(sParam,defaultVal) {
 }
 
 
-jQuery.getJSON(GetQueryStringParams("config","/templates/usa_website/course_map/config.json"), function(data, textStatus, jqXHR) {
+jQuery.getJSON(GetQueryStringParams("config","course_map/config.json"), function(data, textStatus, jqXHR) {
 	config=data;
 	
 	if (config.type!="network") {
